b0723ca288e33d1d3f4df07e558af322a0da2083
Ritish369 <ritish2316@gmail.com>
2024-06-03 17:21:51 +0530

    Added more enjoyable  functionality

--- guess_game.py
+++ guess_game.py
@@ -1,6 +1,5 @@
 from random import choice
 
-
 def main():
     initializer()
     user_guess()
@@ -8,20 +7,23 @@ def main():
 
 def initializer():
     global animals, secret_word, guess
-    animals = ["Cow", "Dog", "Cat"]
+    animals = ["Cow", "Dog", "Cat", "Lion"]
     secret_word = choice(animals)
     guess = ""
     print("Let's play a guessing game!")
 
 
 def user_guess():
-    while True:
+    lives = 3
+    while True and lives:
         guess = input("Enter your guessed animal: ").title()
         if guess == secret_word:
             print("Got the animal correctly!")
             break
         else:
-            print("Nah! Take another chance.")
+            lives -= 1
+    if not lives:
+        print("You lost!")
 
 
 if __name__ == "__main__":
